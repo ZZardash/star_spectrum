c24c319a538a5ab54a80b7e2effe2dc5419af2a3
Sertaç Yurtaslan <60033511+ZZardash@users.noreply.github.com>
2023-06-18 11:57:47 +0300

    Update plot.py
    comment lines added.

--- plot.py
+++ plot.py
@@ -1,20 +1,20 @@
 import json
 import matplotlib.pyplot as plt
 
-# JSON dosyasını yükleme
+# Loading JSON file
 with open('avg_result.json') as file:
     data = json.load(file)
 
-# "tayf" ve "avg" verilerini ayırma
+# separating "spectrum" and "avg" data
 tayf = [d["tayf"] for d in data]
 avg = [d["avg"] for d in data]
 
-# "tayf" verilerini küçükten büyüğe sıralama
+# Sorting "spectrum" data from smallest to largest
 sorted_indices = sorted(range(len(tayf)), key=lambda k: tayf[k])
 sorted_tayf = [tayf[i] for i in sorted_indices]
 sorted_avg = [avg[i] for i in sorted_indices]
 
-# Grafiği oluşturma
+# Create the graph
 plt.scatter(sorted_tayf, sorted_avg)
 plt.xlabel('Tayf')
 plt.ylabel('avg')
